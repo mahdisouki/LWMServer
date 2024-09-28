a55093701f3e6cc305013683e7ababe6a36e5e93
AmenyElokb7 <amenyelokb@gmail.com>
2024-09-28 01:34:53 +0100

    PW-API-01 : Update listing tasks

--- controllers/staffCtrl.js
+++ controllers/staffCtrl.js
@@ -23,52 +23,53 @@ const isWithinDistance = (coord1, coord2, maxDistance) => {
   return distance <= maxDistance;
 };
 
-class APIfeatures {
-  constructor(query, queryString) {
-    this.query = query;
-    this.queryString = queryString;
-    this.total = 0;
-  }
-  async filtering() {
-    const queryObj = { ...this.queryString };
-    const excludedFields = ["page", "sort", "limit"];
-    excludedFields.forEach((el) => delete queryObj[el]);
+// class APIfeatures {
+//   constructor(query, queryString) {
+//     this.query = query;
+//     this.queryString = queryString;
+//     this.total = 0;
+//   }
+//   async filtering() {
+//     const queryObj = { ...this.queryString };
+//     const excludedFields = ["page", "sort", "limit"];
+//     excludedFields.forEach((el) => delete queryObj[el]);
 
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(
-      /\b(gte|gt|lt|lte|regex)\b/g,
-      (match) => "$" + match
-    );
+//     let queryStr = JSON.stringify(queryObj);
+//     queryStr = queryStr.replace(
+//       /\b(gte|gt|lt|lte|regex)\b/g,
+//       (match) => "$" + match
+//     );
 
-    let countQuery = this.query.clone();
+//     let countQuery = this.query.clone();
 
-    this.query = this.query.find(JSON.parse(queryStr));
-    countQuery = countQuery.find(JSON.parse(queryStr));
+//     this.query = this.query.find(JSON.parse(queryStr));
+//     countQuery = countQuery.find(JSON.parse(queryStr));
 
-    this.total = await countQuery.countDocuments();
+//     this.total = await countQuery.countDocuments();
 
-    return this;
-  }
+//     return this;
+//   }
 
-  sorting() {
-    if (this.queryString.sort) {
-      const sortBy = this.queryString.sort.split(",").join(" ");
-      this.query = this.query.sort(sortBy);
-    } else {
-      this.query = this.query.sort("-createdAt");
-    }
+//   sorting() {
+//     if (this.queryString.sort) {
+//       const sortBy = this.queryString.sort.split(",").join(" ");
+//       this.query = this.query.sort(sortBy);
+//     } else {
+//       this.query = this.query.sort("-createdAt");
+//     }
 
-    return this;
-  }
-  paginating() {
-    const page = parseInt(this.queryString.page, 10) || 1;
-    const limit = parseInt(this.queryString.limit, 10) || 9;
-    const skip = (page - 1) * limit;
-    this.query = this.query.skip(skip).limit(limit);
+//     return this;
+//   }
+//   paginating() {
+//     const page = parseInt(this.queryString.page, 10) || 1;
+//     const limit = parseInt(this.queryString.limit, 10) || 9;
+//     const skip = (page - 1) * limit;
+//     this.query = this.query.skip(skip).limit(limit);
 
-    return this;
-  }
-}
+//     return this;
+//   }
+// }
+
 const staffManagement = {
   addStaff: async (req, res) => {
     try {
--- controllers/taskCtrl.js
+++ controllers/taskCtrl.js
@@ -1,131 +1,165 @@
 const Task = require("../models/Task");
 const Truck = require("../models/Truck");
 
+// class APIfeatures {
+//   constructor(query, queryString) {
+//     this.query = query;
+//     this.queryString = queryString;
+//   }
+//   filtering() {
+//     const queryObj = { ...this.queryString }; //queryString = req.query
+//     const exludedFileds = ["page", "sort", "limit"];
+//     exludedFileds.forEach((el) => delete queryObj[el]);
+//     let queryStr = JSON.stringify(queryObj);
+//     queryStr = queryStr.replace(
+//       /\b(gte|gt|lt|lte|regex)\b/g,
+//       (match) => "$" + match
+//     );
+//     this.query.find(JSON.parse(queryStr));
+//     return this;
+//   }
+//   sorting() {
+//     if (this.queryString.sort) {
+//       const sortBy = this.queryString.sort.split(",").join(" ");
+//       console.log(sortBy);
+//       this.query = this.query.sort(sortBy);
+//     } else {
+//       this.query = this.query.sort("-createdAt");
+//     }
+//     return this;
+//   }
+//   paginating() {
+//     const page = this.queryString.page * 1 || 1;
+//     const limit = this.queryString.limit * 1 || 9;
+//     const skip = (page - 1) * limit;
+//     this.query = this.query.skip(skip).limit(limit);
+//     return this;
+//   }
+// }
 
-
-class APIfeatures {
-    constructor(query , queryString){
-        this.query=query;
-        this.queryString= queryString;
-    }
-    filtering(){
-       const queryObj = {...this.queryString} //queryString = req.query
-       const exludedFileds = ['page', 'sort' , 'limit']
-       exludedFileds.forEach(el => delete(queryObj[el]))
-       let queryStr = JSON.stringify(queryObj)
-       queryStr = queryStr.replace(/\b(gte|gt|lt|lte|regex)\b/g , match => '$' + match)
-       this.query.find(JSON.parse(queryStr))
-        return this ;
-    }
-    sorting(){
-        if(this.queryString.sort){
-            const sortBy = this.queryString.sort.split(',').join(' ')
-            console.log(sortBy)
-            this.query = this.query.sort(sortBy)
-        }else{
-            this.query = this.query.sort('-createdAt')
-        }
-        return this ; 
-    }
-    paginating(){ 
-        const page = this.queryString.page * 1 || 1
-        const limit = this.queryString.limit * 1 || 9
-        const skip = (page-1) * limit
-        this.query = this.query.skip(skip).limit(limit )
-         return this ; 
-    }
-}
 const taskCtrl = {
   createTask: async (req, res) => {
     try {
-        const {
-            firstName, lastName, phoneNumber,
-            location, date, hour, object, price, paymentStatus
-        } = req.body;
+      const {
+        firstName,
+        lastName,
+        phoneNumber,
+        location,
+        date,
+        hour,
+        object,
+        price,
+        paymentStatus,
+      } = req.body;
 
-        const clientObjectPhotos = req.files.map(file => file.path);
+      const clientObjectPhotos = req.files.map((file) => file.path);
 
-        const newTask = new Task({
-            firstName,
-            lastName,
-            phoneNumber,
-            location,
-            clientObjectPhotos, 
-            date,
-            hour,
-            object,
-            price,
-            paymentStatus, 
-            taskStatus: "Processing"
-        });
+      const newTask = new Task({
+        firstName,
+        lastName,
+        phoneNumber,
+        location,
+        clientObjectPhotos,
+        date,
+        hour,
+        object,
+        price,
+        paymentStatus,
+        taskStatus: "Processing",
+      });
 
-        await newTask.save();
-        res.status(201).json({ message: "Task created successfully", task: newTask });
+      await newTask.save();
+      res
+        .status(201)
+        .json({ message: "Task created successfully", task: newTask });
     } catch (error) {
-        res.status(400).json({ message: "Failed to create task", error: error.message });
+      res
+        .status(400)
+        .json({ message: "Failed to create task", error: error.message });
     }
-},
-// GET TASK BY ID 
-getTaskById: async (req, res) => {
+  },
+  // GET TASK BY ID
+  getTaskById: async (req, res) => {
     const { taskId } = req.params;
     try {
-        const task = await Task.findById(taskId);
-        if (!task) {
-            return res.status(404).json({ message: "Task not found" });
-        }
-        res.status(200).json({ message: "Task retrieved successfully", task });
+      const task = await Task.findById(taskId);
+      if (!task) {
+        return res.status(404).json({ message: "Task not found" });
+      }
+      res.status(200).json({ message: "Task retrieved successfully", task });
     } catch (error) {
-        res.status(500).json({ message: "Failed to retrieve task", error: error.message });
-    }
-},
-
-getAllTasks: async (req, res) => {
-    try {
-        const features = new APIfeatures(Task.find(), req.query)
-      .filtering()
-      .sorting()
-      .paginating();
-      const tasks = await features.query ; // Fetch all tasks
-      res.status(200).json({ message: "All tasks retrieved successfully", tasks });
-    } catch (error) {
-      res.status(500).json({ message: "Failed to retrieve tasks", error: error.message });
+      res
+        .status(500)
+        .json({ message: "Failed to retrieve task", error: error.message });
     }
   },
 
-assignTruckToTask: async (req, res) => {
-    const { taskId } = req.params; 
-    const { truckName } = req.body; 
+  getAllTasks: async (req, res) => {
+    try {
+      const query = Task.find();
+      const { page, limit, filters } = req.query;
+
+      const total = await Task.countDocuments(query);
+
+      if (filters) {
+        query = query.find(filters);
+      }
+
+      const tasks = await query
+        .skip((page - 1) * limit)
+        .limit(limit)
+        .exec();
+
+      res.status(200).json({
+        message: "All tasks retrieved successfully",
+        tasks,
+        meta: {
+          currentPage: Number(page),
+          limit: Number(limit),
+          total,
+          count: tasks.length,
+        },
+      });
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: "Failed to retrieve tasks", error: error.message });
+    }
+  },
+
+  assignTruckToTask: async (req, res) => {
+    const { taskId } = req.params;
+    const { truckName } = req.body;
 
     try {
-        
-        const truck = await Truck.findOne({ name: truckName });
-        if (!truck) {
-            return res.status(404).json({ message: "Truck not found" });
-        }
+      const truck = await Truck.findOne({ name: truckName });
+      if (!truck) {
+        return res.status(404).json({ message: "Truck not found" });
+      }
 
-     
-        const updatedTask = await Task.findByIdAndUpdate(
-            taskId,
-            { $set: { truckId: truck._id } }, 
-            { new: true }
-        );
+      const updatedTask = await Task.findByIdAndUpdate(
+        taskId,
+        { $set: { truckId: truck._id } },
+        { new: true }
+      );
 
-        if (!updatedTask) {
-            return res.status(404).json({ message: "Task not found" });
-        }
+      if (!updatedTask) {
+        return res.status(404).json({ message: "Task not found" });
+      }
 
-      
-        truck.tasks.push(updatedTask._id);
-        await truck.save(); 
+      truck.tasks.push(updatedTask._id);
+      await truck.save();
 
-        res.status(200).json({ message: "Truck assigned to task successfully", task: updatedTask });
+      res.status(200).json({
+        message: "Truck assigned to task successfully",
+        task: updatedTask,
+      });
     } catch (error) {
-        res.status(500).json({ message: "Failed to assign truck", error: error.message });
+      res
+        .status(500)
+        .json({ message: "Failed to assign truck", error: error.message });
     }
-},
-
-
-
+  },
 };
 
 module.exports = taskCtrl;
