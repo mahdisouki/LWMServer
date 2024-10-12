35e518d9e7d7f03ac0f5097c9c8c51d1ddf286a5
AmenyElokb7 <amenyelokb@gmail.com>
2024-10-12 17:25:08 +0100

    TIPPING ROUTES

---
+++ routes/tipping.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const tippingCtrl = require('../controllers/tippingCtrl');
+const { isAuth } = require('../middlewares/auth');
+const { checkRole } = require('../middlewares/role');
+
+router.post('/create-tipping', isAuth, checkRole('Driver', 'Helper'), tippingCtrl.createTippingRequest);
+router.get('/tipping', isAuth, checkRole('Driver', 'Helper'), tippingCtrl.getAllTippingRequestsForUser);
+router.get('/tipping/:id', isAuth, checkRole('Driver', 'Helper'), tippingCtrl.getTippingRequestById);
+
+router.get('/tipping', tippingCtrl.getAllTippingRequestsForAdmin);
+
+router.get('/tipping/:id', isAuth, checkRole('Admin'), tippingCtrl.getTippingRequestById);
+router.put('/tipping/:id', isAuth, checkRole('Admin'), tippingCtrl.updateTippingRequest);
+router.delete('/tipping/:id', isAuth, checkRole('Admin'), tippingCtrl.deleteTippingRequest);
+router.put('/tipping/:id/validate', isAuth, checkRole('Admin'), tippingCtrl.updateTippingRequestStatus);
+
+
+module.exports = router;
