8f1395b497a4e907199f0e73cc60567175412a77
EyaTriki <eya2000triki@gmail.com>
2024-11-09 23:36:58 +0100

    Résolution de conflit

--- models/StandardItem.js
+++ models/StandardItem.js
@@ -4,13 +4,8 @@ const standardItemSchema = new mongoose.Schema({
     itemName: { type: String, required: true },
     image: { type: String, required: true },
     price: { type: Number, required: true },
-    category: { type: String, required: true },
+    category: { type: [String], required: true }, 
     description: { type: String, required: false },
-
-    // additionalFees: [{
-    //     condition: String,
-    //     amount: Number,
-    // }]
 });
 
 const StandardItem = mongoose.model('StandardItem', standardItemSchema);
