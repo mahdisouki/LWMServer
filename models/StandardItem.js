ba53a0a67228ad4612f6248632b7eb63957a962a
mahdisouki <100594801+mahdisouki@users.noreply.github.com>
2024-11-17 00:16:14 +0100

    adding serviceCategory

--- models/StandardItem.js
+++ models/StandardItem.js
@@ -4,7 +4,7 @@ const standardItemSchema = new mongoose.Schema({
     itemName: { type: String, required: true },
     image: { type: String, required: true },
     price: { type: Number, required: true },
-    category: { type: [String], required: true }, 
+    category: [{type:mongoose.Types.ObjectId , ref:"serviceCategory"}], 
     description: { type: String, required: false },
 });
 
---
+++ routes/serviceCategory.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const multer = require('multer');
+const multer = require('../middlewares/multer');
+const serviceCategoryCtrl = require('../controllers/serviceCategoryCtrl');
+const router = express.Router();
+
+router.post('/service-categories' ,multer.single('image'), serviceCategoryCtrl.createServiceCategory);
+router.get('/service-categories', serviceCategoryCtrl.getAllServiceCategories);
+router.get('/service-categories/:id', serviceCategoryCtrl.getServiceCategoryById);
+router.put('/service-categories/:id' ,multer.single('image'), serviceCategoryCtrl.updateServiceCategory);
+router.delete('/service-categories/:id', serviceCategoryCtrl.deleteServiceCategory);
+
+module.exports = router;
