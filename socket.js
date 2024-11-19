6f00efa93747ce0b70ae6cbfe9ca60f0bb0737e8
mahdisouki <100594801+mahdisouki@users.noreply.github.com>
2024-11-19 21:21:26 +0100

    .

--- socket.js
+++ socket.js
@@ -19,6 +19,7 @@ const initSocket = (server) => {
         'https://localhost:5173',
         'http://localhost:5174',
         'https://localhost:5173',
+        'https://dirverapp.netlify.app'
       ],
       methods: ['GET', 'POST'],
       credentials: true,
