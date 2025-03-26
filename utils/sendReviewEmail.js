d5e8cca1bb2afa13cd80ce64219fda52a06ab884
mahdisouki <100594801+mahdisouki@users.noreply.github.com>
2025-03-26 05:27:41 +0100

    updates

--- utils/sendReviewEmail.js
+++ utils/sendReviewEmail.js
@@ -9,7 +9,7 @@ const sendReviewRequestEmail = async ({ email, firstName, orderId }) => {
     },
   });
 
-  const internalReviewBase = `https://www.londonwastemanagement.com/customer-review/?order_id=${orderId}`;
+  const internalReviewBase = `https://lwmadmin.netlify.app/review/${orderId}`;
   const trustpilotLink = `https://www.trustpilot.com/evaluate/www.londonwastemanagement.com`;
 
   const mailOptions = {
@@ -17,39 +17,65 @@ const sendReviewRequestEmail = async ({ email, firstName, orderId }) => {
     to: email,
     subject: "How Was Your Experience? | London Waste Management",
     html: `
-      <div style="font-family: Arial, sans-serif; max-width: 700px; margin: auto; padding: 40px; border: 1px solid #ddd;">
-        <h2 style="color: #333; text-align: center;">Customer Review</h2>
-        <p style="text-align:center; font-weight: 600;">How Was Your Experience?</p>
-        
-        <div style="display: flex; flex-direction: column; align-items: center; gap: 15px; margin: 30px 0;">
+  <div style="font-family: Arial, sans-serif; max-width: 700px; margin: auto; padding: 40px; border: 1px solid #ddd;backgroundcolor:#b1b1b1">
+    <div style="align-items:center">
+    <img src="https://res.cloudinary.com/dfxeaeebv/image/upload/v1742959873/slpany1oqx09lxj72nmd.png"  />
+    </div>
+    <h2 style="color: #333; text-align: center;background-color:#8dc044">Customer Review</h2>
+    <p style="text-align:right; font-weight: 600;">Hi ${firstName}
+Thank you for choosing LWM for your recent removal and recycling collection.
+We would love it if you could help us and other customers by rating your
+experience with us. It only takes a few clicks and would be sincerely
+appreciated!
+Kind regards,</p>
 
-          <!-- 1 STAR -->
-          <a href="${internalReviewBase}&rating=1" style="text-decoration: none;">
-            <img src="https://londonwaste.duckdns.org/logo/1-star.webp" alt="1 star review" style="max-width: 300px;">
+    <table width="100%" cellpadding="10" cellspacing="0" style="text-align: center; margin: 30px 0;">
+      <tr>
+        <td>
+          <a href="${internalReviewBase}&rating=1">
+            <img src="https://res.cloudinary.com/dfxeaeebv/image/upload/v1742958518/tkhlpz8mjob3ulpbhmli.webp" width="30" />
+            <img src="https://res.cloudinary.com/dfxeaeebv/image/upload/v1742958518/jfwffhogxomlk1z8wrbi.webp" width="30" />
+            <img src="https://res.cloudinary.com/dfxeaeebv/image/upload/v1742958518/jfwffhogxomlk1z8wrbi.webp" width="30" />
+            <img src="https://res.cloudinary.com/dfxeaeebv/image/upload/v1742958518/jfwffhogxomlk1z8wrbi.webp" width="30" />
+            <img src="https://res.cloudinary.com/dfxeaeebv/image/upload/v1742958518/jfwffhogxomlk1z8wrbi.webp" width="30" />
           </a>
-
-          <!-- 3 STARS -->
-          <a href="${internalReviewBase}&rating=3" style="text-decoration: none;">
-            <img src="https://londonwaste.duckdns.org/logo/1-star.webp" alt="3 stars review" style="max-width: 300px;">
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <a href="${internalReviewBase}&rating=3">
+            <img src="https://res.cloudinary.com/dfxeaeebv/image/upload/v1742958518/jcnlv0d7qa3cgjikanw0.webp" width="30" />
+            <img src="https://res.cloudinary.com/dfxeaeebv/image/upload/v1742958518/jcnlv0d7qa3cgjikanw0.webp" width="30" />
+            <img src="https://res.cloudinary.com/dfxeaeebv/image/upload/v1742958518/jcnlv0d7qa3cgjikanw0.webp" width="30" />
+            <img src="https://res.cloudinary.com/dfxeaeebv/image/upload/v1742958518/jfwffhogxomlk1z8wrbi.webp" width="30" />
+            <img src="https://res.cloudinary.com/dfxeaeebv/image/upload/v1742958518/jfwffhogxomlk1z8wrbi.webp" width="30" />
           </a>
-
-          <!-- 5 STARS - trustpilot -->
-          <a href="${trustpilotLink}" target="_blank" style="text-decoration: none;">
-            <img src="https://londonwaste.duckdns.org/logo/1-star.webp" alt="5 stars review" style="max-width: 300px;">
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <a href="${trustpilotLink}" target="_blank">
+            <img src="https://res.cloudinary.com/dfxeaeebv/image/upload/v1742958518/ac4cq9liiy5klo9d3bew.webp" width="30" />
+            <img src="https://res.cloudinary.com/dfxeaeebv/image/upload/v1742958518/ac4cq9liiy5klo9d3bew.webp" width="30" />
+            <img src="https://res.cloudinary.com/dfxeaeebv/image/upload/v1742958518/ac4cq9liiy5klo9d3bew.webp" width="30" />
+            <img src="https://res.cloudinary.com/dfxeaeebv/image/upload/v1742958518/ac4cq9liiy5klo9d3bew.webp" width="30" />
+            <img src="https://res.cloudinary.com/dfxeaeebv/image/upload/v1742958518/ac4cq9liiy5klo9d3bew.webp" width="30" />
           </a>
+        </td>
+      </tr>
+    </table>
 
-        </div>
+    <p style="text-align:center; font-size: 14px; color: #777;">Click on the stars to rate your experience.</p>
 
-        <p style="text-align:center; font-size: 14px; color: #777;">Click on the stars to rate your experience.</p>
+    <hr style="margin: 40px 0;">
 
-        <hr style="margin: 40px 0;">
-
-        <footer style="text-align: center; font-size: 12px; color: #888;">
-          <p>London Waste Management</p>
-          <p>Email: hello@londonwastemanagement.com</p>
-        </footer>
-      </div>
-    `,
+    <footer style="text-align: center; font-size: 12px; color: #888;">
+      <p>London Waste Management</p>
+      <p>Email: hello@londonwastemanagement.com</p>
+    </footer>
+  </div>
+`
+,
   };
 
   try {
